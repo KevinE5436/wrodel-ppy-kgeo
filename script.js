194d19e9e77f49b08a012bf2a32f32aa9e6552fd
KevinE5436 <151643326+KevinE5436@users.noreply.github.com>
2023-11-28 16:34:33 -0300

    wrodle

---
+++ script.js
@@ -0,0 +1,14 @@
+let intentos = 6;
+let palabra = "APPLE";
+
+
+
+
+
+
+function leerIntento(){
+    let intento = document.getElementById("guess-input");
+    intento = intento.value;
+    intento = intento.toUpperCase(); 
+    return intento;
+}
